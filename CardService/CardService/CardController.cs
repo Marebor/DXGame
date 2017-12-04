b996505244fba9ad5fa595f901c116edbc2dc59c
Marebor <1marek.borkowski1@gmail.com>
2017-12-04 13:59:40 +0100

    Started-Card-Service

---
+++ CardService/CardService/CardController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Web;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+using FileStorage;
+
+namespace CardService
+{
+    public class CardController : ApiController
+    {
+        private FileStorageFolder CardsFolder = new FileStorageFolder(Path.Combine(Directory.GetCurrentDirectory(), "Content", "Cards"));
+
+        private const string CARDS_DIRECTORY = "Content/Files";
+        
+        public IEnumerable<int> Get()
+        {
+            return CardsFolder.Files.Select(f => f.ID);
+        }
+        public string Get(int id)
+        {
+            var card = CardsFolder.Files.FirstOrDefault(f => f.ID == id);
+            if (card == null) throw new FileNotFoundException();
+
+            return card.Path;
+        }
+
+        public void Post()
+        {
+
+        }
+    }
+}

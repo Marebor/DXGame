20d941108d5f82d1930b438bcdb6798bed7dd720
marebor <1marek.borkowski1@gmail.com>
2018-03-13 22:58:41 +0100

    fix

--- src/DXGame.ReadModel/Infrastructure/Mapper.cs
+++ src/DXGame.ReadModel/Infrastructure/Mapper.cs
@@ -5,6 +5,11 @@ namespace DXGame.ReadModel.Infrastructure
     public class Mapper : Abstract.IMapper
     {
         IMapper _mapper;
+
+        public Mapper(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
         public T Map<T>(object from)
             => _mapper.Map<T>(from);
     }

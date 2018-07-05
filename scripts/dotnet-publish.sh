63c4ea218a3295973ab17a96cd78043759855964
marebor <1marek.borkowski1@gmail.com>
2018-07-05 08:59:12 +0200

    building readmodel, refactoring API

--- scripts/dotnet-publish.sh
+++ scripts/dotnet-publish.sh
@@ -1,5 +1,7 @@
 cd ../src
 dotnet clean ./DXGame.Api -c Release
 dotnet publish ./DXGame.Api -c Release -o ./bin/Docker --force
+dotnet clean ./DXGame.ReadModel -c Release
+dotnet publish ./DXGame.ReadModel -c Release -o ./bin/Docker --force
 dotnet clean ./DXGame.Services.Playroom -c Release
 dotnet publish ./DXGame.Services.Playroom -c Release -o ./bin/Docker --force
